7bd043efab3e5eaa6071d007751d08bf8b6b4836
Gustavo Andrade <gustavoandrade0410@gmail.com>
2026-03-02 13:50:34 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function togglemode () {
+  const html = document.documentElement 
+  
+  html.classList.toggle('light')
+  /*if(html.classList.contains('light')) { 
+    html.classList.remove('light')
+  } else {
+    html.classList.add('light') 
+  }*/  /*essa função faz exatamente oq a primeira está fazendo*/ 
+  
+  /*pegar a tag img*/
+  /* const img = document.querySelector ("#profile img")*/
+
+  /*substituir a imagem ao trocar para o modo claro*/ 
+  /*if(html.classList.contains('light')) { 
+    img.setAttribute('src', './assets/avatar-alternativo.png')}*/
+
+ /*manter caso esteja no modo escuro*/  
+  /*else {
+    img.setAttribute('src', './assets/avatar.png')
+    }*/ 
+  /*não vou fazer mas vou deixar anotado para aprender*/ 
+
+}
